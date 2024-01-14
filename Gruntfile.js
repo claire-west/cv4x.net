6743a4c5b4ea1b51088ad5f274696c870528fbf7
Claire West <bokthaku@gmail.com>
2024-01-14 11:20:39 -0500

    JS preloads and new deploy workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,9 +31,46 @@ module.exports = function(grunt) {
         expand: true
       }
     },
+    uglify: {
+      app: {
+        expand: true,
+        src: [ 'js/**/*.js' ],
+        dest: 'build'
+      },
+      frag: {
+        expand: true,
+        src: [ 'frag/**/*.js' ],
+        dest: 'build'
+      }
+    },
+    js_preload: {
+      app: {
+        options: {
+          dest: 'js/',
+          namespace: 'app'
+        },
+        cwd: 'build',
+        src: [ 'js/**/*.js' ],
+        expand: true
+      },
+      frag: {
+        options: {
+          dest: 'frag/js/',
+          namespace: 'frag'
+        },
+        cwd: 'build',
+        src: [ 'frag/**/*.js' ],
+        expand: true
+      }
+    },
     watch: {
       testapp: {
-        files: [ 'frag/**/*.html' ],
+        files: [
+          'frag/**/*.html',
+          'template/**',
+          'js/**/*.js',
+          'frag/**/*.js',
+          '!frag/blog/*' ],
         tasks: [ 'preload' ],
         options: {
           spawn: false,
@@ -44,7 +81,8 @@ module.exports = function(grunt) {
 
   grunt.loadTasks('./grunt');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', [ 'copy' ]);
-  grunt.registerTask('preload', [ 'fragment_preload' ]);
+  grunt.registerTask('default', [ 'preload', 'copy' ]);
+  grunt.registerTask('preload', [ 'fragment_preload', 'uglify:app', 'uglify:frag', 'js_preload:app', 'js_preload:frag' ]);
 };
